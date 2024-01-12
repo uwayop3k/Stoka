4e4f90492bf107852222d137469cd3dd113d99eb
pa3ck <patrick.uwayo250@gmail.com>
2024-01-12 12:14:15 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// let text = "";
+// for (let i = 0; i < cars.length; i++) {
+//   text += cars[i] + "<br>";
+// }
+
+// document.getElementById("demo").innerHTML = text;
+
+let resultHTML = `
+    <div class="results">
+        <div class="product-out">
+            Product:
+        </div>
+        <div class="branch-out">
+            Branch:
+        </div>
+        <div class="total-purchase">
+            Total purchase:
+        </div>
+        <div class="total-sale">
+            Total sales:
+        </div>
+        <div class="actual-oosd">
+            Actual OOSD:
+        </div>
+        <div class="average-oosd">
+            Average OOSD:
+        </div>
+    </div>
+`;
+let resultsHTML = "";
+for (let i = 0; i < 20; i++) {
+    resultsHTML += resultHTML + "<br>";
+  }
+document.getElementById("outputs").innerHTML = resultsHTML;
+
